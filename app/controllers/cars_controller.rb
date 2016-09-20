68464971f7dba3e8f30459d15c0a2faf98c99cf4
Alex <alex@alex-VirtualBox.(none)>
2016-09-20 19:05:39 +0300

    cars controller + comments + rubyStyle

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,28 @@
 class CarsController < ApplicationController
   def index
+    @cars = Car.all
 
+    # Обрабатываем параметр запроса:
+    # - удаляем ненужные ненужные элементы запроса
+    # - составляем массив
+    user_position = params[:position].delete('a-zA-Z[] ').split(',')
+
+    # Заполняем массив рассчитанными ETA для каждой свободной машины
+    cars_eta = []
+    @cars.each do |car|
+      cars_eta << car.eta(user_position) if car.available
+    end
+
+    # Сортируем получившийся массив
+    cars_eta.sort!
+
+    # и выбираем первые три (ближайшие) машины
+    average_eta = 0
+    3.times do |i|
+      average_eta += cars_eta[i]
+    end
+
+    # Наконец, рендерим JSON с единственным значением среднего времени подачи машины в минутах
+    render json: (average_eta / 3).round
   end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,16 +1,22 @@
 class Car < ActiveRecord::Base
-  def eta
-    user_position = [59.68585959, 30.44572771]
+
+  # Если position и available не заданы, объект не будет сохранен в базу
+  validates :position, :available, presence: true
+
+  # Расчитываем примерное время подачи данной машины
+  def eta(user_position)
     car_position = self.position.split(' ')
 
-    lat1 = car_position[0].to_f/180 * Math::PI
-    long1 = car_position[1].to_f/180 * Math::PI
-    lat2 = user_position[0]/180 * Math::PI
-    long2 = user_position[1]/180 * Math::PI
     r = 6371
-    sin1 = Math.sin((lat1 - lat2)/2)
-    sin2 = Math.sin((long1 - long2)/2)
-    eta =1.5*2*r*Math.asin(Math.sqrt((sin1**2) + (Math.cos(lat1)*Math.cos(lat2)*(sin2**2))))
-    eta.round(3)
+
+    # Переводим градусы в радианы
+    lat1 = car_position[0].to_f / 180 * Math::PI
+    long1 = car_position[1].to_f / 180 * Math::PI
+    lat2 = user_position[0].to_f / 180 * Math::PI
+    long2 = user_position[1].to_f / 180 * Math::PI
+
+    sin1 = Math.sin((lat1 - lat2) / 2)
+    sin2 = Math.sin((long1 - long2) / 2)
+    eta = 1.5 * 2 * r * Math.asin(Math.sqrt((sin1**2) + (Math.cos(lat1) * Math.cos(lat2) * (sin2**2))))
   end
 end
